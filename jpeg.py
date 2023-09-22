f73e154cb816c6f673931d99f4751e4e15abf979
CJ <131416165+cjschultz87@users.noreply.github.com>
2023-09-22 19:39:38 -0400

    Create jpeg.py

---
+++ jpeg.py
@@ -0,0 +1,89 @@
+import sys
+
+try:
+    foxtrot = open(sys.argv[1],"r")
+except:
+    print "File does not exist"
+    
+    
+bytes = []
+
+bytes_L = 0
+
+while True:
+    alpha = foxtrot.readline()
+    
+    bytes_L += 1
+    
+    if alpha == "":
+        break
+    
+    bytes.append(alpha[0:2])
+    
+m16 = "cdef"
+
+tags = []
+
+hex = "0123456789abcdef"
+
+for element in m16:
+    for element_1 in hex:
+        tags.append(element + element_1)
+        
+tags.pop(len(tags)-1)
+
+index = 0
+
+alpha = 0
+
+while alpha == 0 and index < bytes_L:
+    if bytes[index] == "ff":
+        print index
+        index += 1
+        
+        segment_val = 0
+        
+        if bytes[index] in tags[16:25]:
+            index += 1
+        
+        elif (bytes[index] in tags) and not(bytes[index] in tags[16:26]):
+            if bytes[index] == "da":
+                print "start of scan: byte " + str(index)
+                bravo = 0
+                
+                while bravo == 0:
+                    index += 1
+                    
+                    if bytes[index] == "ff":
+                        index += 1
+                        
+                        if bytes[index] in tags and not(bytes[index] in tags[16:26]):
+                            index -= 1
+                            bravo = 1
+                            
+                        if bytes[index] == "d9":
+                            alpha = 1
+                            
+                            break
+                
+            else:
+                print bytes[index-1] + bytes[index] + ": byte " + str(index)
+            
+                def sierraF(bravo):
+                    sierra_0 = hex.index(bravo[0])
+                    sierra_1 = hex.index(bravo[1])
+                    return sierra_0*16 + sierra_1
+            
+                index += 1
+            
+                segment_val += 256 * sierraF(bytes[index])
+            
+                index += 1
+            
+                segment_val += sierraF(bytes[index])
+            
+                segment_val -= 1
+            
+        index += segment_val
+        
+print str(index + 1) + " bytes"
